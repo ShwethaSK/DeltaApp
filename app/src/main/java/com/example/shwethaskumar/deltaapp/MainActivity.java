0375bc0c08d233a779903b14a12dd9ffda367379
Shwetha Kumar <swethakumar6@gmail.com>
2017-06-05 03:04:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shwethaskumar/deltaapp/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.shwethaskumar.deltaapp;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int value_r=0,value_g=0,value_b=0;
+    TextView changingValue_r,changingValue_g,changingValue_b;
+    LinearLayout lineLay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        lineLay=(LinearLayout) findViewById(R.id.linearLayout);
+        changingValue_r=(TextView) findViewById(R.id.value_red);
+        changingValue_r.setText(String.valueOf(value_r));
+        changingValue_g=(TextView) findViewById(R.id.value_green);
+        changingValue_g.setText(String.valueOf(value_g));
+        changingValue_b=(TextView) findViewById(R.id.value_blue);
+        changingValue_b.setText(String.valueOf(value_b));
+        setSupportActionBar(toolbar);
+            }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void increaseValueRed(View view)
+    {
+        value_r=value_r+1;
+        if(value_r>=255)
+            value_r=0;
+        changingValue_r.setText(String.valueOf(value_r));
+        setColor();
+    }
+
+    public void increaseValueGreen(View view)
+    {
+        value_g=value_g+1;
+        if(value_g>=255)
+            value_g=0;
+        changingValue_g.setText(String.valueOf(value_g));
+        setColor();
+    }
+    public void increaseValueBlue(View view)
+    {
+        value_b=value_b+1;
+        if(value_b>=255)
+            value_b=0;
+        changingValue_b.setText(String.valueOf(value_b));
+        setColor();
+    }
+    public void setColor()
+    {
+        lineLay.setBackgroundColor(Color.rgb(value_r,value_g,value_b));
+    }
+}
